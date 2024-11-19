dbaa3f84d8ade3a79a965375a3204f4ade9658ff
powerfuljackell <johnsonjakel@aol.com>
2024-11-18 18:32:50 -0700

    Completed

--- code.js
+++ code.js
@@ -1,3 +1,25 @@
 function binarySearch(list, element) {
+
+    var start = 0;
+    var end = list.length - 1;
+    let mid;
+    while (end >=start) {
+         mid =start+ Math.floor((end - start) / 2); //appears that I wrote this wrong, resulting in infinite loops in the future
+  
+
+        if (list[mid] == element){
+            //while(list[mid-1] == list[mid]) mid--; //used to return "first" instance of the value's position
+            if(list[0] == element) mid = 0; //worked on with Aaron Krapes to find a more efficient solution
+            return mid;
+        }
+        
+
+        if (list[mid] > element)
+           end = mid - 1;
+             
+        else
+           start = mid + 1;
+    }
+  
     return -1;
 }
